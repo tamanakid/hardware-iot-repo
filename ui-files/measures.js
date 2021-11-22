ad0fbe7bf43de7197f6816c7b9c14902986987b0
tamanakid <nfvillanuevap@gmail.com>
2021-11-22 09:40:01 +0100

    Further advances in measurements

--- ui-files/measures.js
+++ ui-files/measures.js
@@ -8,56 +8,67 @@ async function renderMeasurementValues () {
     if (!isAJAXCallInProgress) {
         isAJAXCallInProgress = true;
 
-        let loaderTemperatureEl;
+        let loaderElements;
         if (isFirstCall) {
-            loaderTemperatureEl = document.getElementById('temperature_content_info');
-            loaderTemperatureEl.appendChild(renderLoader('large'));
+            loaderElements = document.getElementsByClassName('content_info');
+            loaderElements[0].appendChild(renderLoader('large'));
+            loaderElements[1].appendChild(renderLoader('large'));
         } else {
-            loaderTemperatureEl = document.getElementById('loader_temp_current');
-            loaderTemperatureEl.appendChild(renderLoader('small'));
+            loaderElements = document.getElementsByClassName('loader_current');
+            loaderElements[0].appendChild(renderLoader('small'));
+            loaderElements[1].appendChild(renderLoader('small'));
         }
     
         const measurements = await endpoints.getMeasurementValues();
     
         if (isFirstCall) {
-            loaderTemperatureEl.classList.remove('content_info--hide-measures');
-            const firstCallLoader = loaderTemperatureEl.getElementsByClassName('loader')[0];
-            firstCallLoader.remove();
+            for (let i = 0; i < loaderElements.length; i++) {
+                loaderElements[i].classList.remove('content_info--hide-measures');
+                const firstCallLoader = loaderElements[i].getElementsByClassName('loader')[0];
+                firstCallLoader.remove();
+            }
             isFirstCall = false;
         } else {
-            loaderTemperatureEl.innerHTML = '';
+            loaderElements[0].innerHTML = '';
+            loaderElements[1].innerHTML = '';
         }
         isAJAXCallInProgress = false;
     
-        // Temperature: Current value
-        const temperatureCurrentEl = document.getElementById('measure_temp_current');
-        temperatureCurrentEl.innerHTML = measurements.temperature.current.value;
-        const temperatureCurrentTimestampEl = document.getElementById('measure_temp_current_timestamp');
-        temperatureCurrentTimestampEl.innerHTML = measurements.temperature.current.timestamp;
+        renderTabMeasurements(measurements.temperature, 'temp');
+        renderTabMeasurements(measurements.humidity, 'humidity');
+    }
+}
 
-        // Temperature: Latest means
-        if (measurements.temperature.means) {
-            measurements.temperature.means.forEach((mean, index) => {
-                const temperatureMeanEl = document.getElementById(`measure_temp_mean${index}`);
-                temperatureMeanEl.innerHTML = mean.value;
-                const temperatureMeanTimestampEl = document.getElementById(`measure_temp_mean${index}_timestamp`);
-                temperatureMeanTimestampEl.innerHTML = mean.timestamp;
-            });
-        }
+function renderTabMeasurements(tabMeasurements, type) {
+    
+    // Temperature: Current value
+    const temperatureCurrentEl = document.getElementById(`measure_${type}_current`);
+    temperatureCurrentEl.innerHTML = tabMeasurements.current.value;
+    const temperatureCurrentTimestampEl = document.getElementById(`measure_${type}_current_timestamp`);
+    temperatureCurrentTimestampEl.innerHTML = tabMeasurements.current.timestamp;
 
-        // Temperature: Record max and min
-        if (measurements.temperature.recordMax) {
-            const temperatureMaxEl = document.getElementById('measure_temp_max');
-            temperatureMaxEl.innerHTML = measurements.temperature.recordMax.value;
-            const temperatureMaxTimestampEl = document.getElementById('measure_temp_max_timestamp');
-            temperatureMaxTimestampEl.innerHTML = measurements.temperature.recordMax.timestamp;
-        }
-        if (measurements.temperature.recordMin) {
-            const temperatureMinEl = document.getElementById('measure_temp_min');
-            temperatureMinEl.innerHTML = measurements.temperature.recordMin.value;
-            const temperatureMinTimestampEl = document.getElementById('measure_temp_min_timestamp');
-            temperatureMinTimestampEl.innerHTML = measurements.temperature.recordMin.timestamp;
-        }
+    // Temperature: Latest means
+    if (tabMeasurements.means) {
+        tabMeasurements.means.forEach((mean, index) => {
+            const temperatureMeanEl = document.getElementById(`measure_${type}_mean${index}`);
+            temperatureMeanEl.innerHTML = mean.value;
+            const temperatureMeanTimestampEl = document.getElementById(`measure_${type}_mean${index}_timestamp`);
+            temperatureMeanTimestampEl.innerHTML = mean.timestamp;
+        });
+    }
+
+    // Temperature: Record max and min
+    if (tabMeasurements.recordMax) {
+        const temperatureMaxEl = document.getElementById(`measure_${type}_max`);
+        temperatureMaxEl.innerHTML = tabMeasurements.recordMax.value;
+        const temperatureMaxTimestampEl = document.getElementById(`measure_${type}_max_timestamp`);
+        temperatureMaxTimestampEl.innerHTML = tabMeasurements.recordMax.timestamp;
+    }
+    if (tabMeasurements.recordMin) {
+        const temperatureMinEl = document.getElementById(`measure_${type}_min`);
+        temperatureMinEl.innerHTML = tabMeasurements.recordMin.value;
+        const temperatureMinTimestampEl = document.getElementById(`measure_${type}_min_timestamp`);
+        temperatureMinTimestampEl.innerHTML = tabMeasurements.recordMin.timestamp;
     }
 }
 
